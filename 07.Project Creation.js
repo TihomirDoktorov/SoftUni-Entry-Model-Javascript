7dd30ea9d74a1098d18abaf229c906bd275780fd
Tihomir Doktorov <tihomirdoktorov@gmail.com>
2022-05-18 13:30:36 +0300

    Delete 07.Project Creation.js

--- 07.Project Creation.js
+++
@@ -1,7 +0,0 @@
-function project(input) {
-    let name = String(input[0]);
-    let number = Number(input[1]);
-    let hours = number * 3;
-    console.log(`The architect ${name} will need ${hours} hours to complete ${number} project/s.`)
-}
-project(["George","4"]);
