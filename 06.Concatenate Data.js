d8087114a6d4863e13362c125261bb34f9277494
Tihomir Doktorov <tihomirdoktorov@gmail.com>
2022-05-18 13:30:14 +0300

    Delete 06.Concatenate Data.js

--- 06.Concatenate Data.js
+++
@@ -1,8 +0,0 @@
-function concatenate(input) {
-    let name = input[0];
-    let lastname = input[1];
-    let age = Number(input[2]);
-    let town = input[3];
-    console.log(`You are ${name} ${lastname}, a ${age}-years old person from ${town}.`);
-}
-concatenate(["Maria","Ivanova",20,"Sofia"]);
