b02dbb06679ba99e271ef1f6d9da052000c6cdde
Tihomir Doktorov <tihomirdoktorov@gmail.com>
2022-05-18 13:31:03 +0300

    Delete 09.Yard Greening.js

--- 09.Yard Greening.js
+++
@@ -1,8 +0,0 @@
-function greening(input) {
-    let kvmeters = Number(input[0]);
-    let price = (kvmeters * 7.61);
-    let discount = price*0.18;
-    console.log(`The final price is: ${(price - discount)} lv.`);
-    console.log(`The discount is: ${discount} lv.`);
-}
-greening([150])
